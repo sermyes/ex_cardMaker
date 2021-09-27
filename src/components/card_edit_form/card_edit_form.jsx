479982d61b0f5afeba74585272172fa99dad0d5f
lsh <sermyes@gmail.com>
2021-09-28 02:08:40 +0900

    add card & edit form component

---
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Button from '../button/button';
+import ImageFileInput from '../image_file_input/image_file_input';
+import styles from './card_edit_form.module.css';
+
+const CardEditForm = ({ card }) => {
+    const {
+        name, 
+        company, 
+        title, 
+        email, 
+        message, 
+        theme, 
+        fileName, 
+        fileURL
+    } = card;
+
+    const onSubmit = () => {
+
+    };
+
+    return (
+        <form className={styles.form}>
+            <input className={styles.input} type="text" name="name" value={name} readOnly/>
+            <input className={styles.input} type="text" name="company" value={company} readOnly/>
+            <select className={styles.select} name="theme" value={theme} readOnly>
+                <option value="light">light</option>
+                <option value="dark">dark</option>
+                <option value="colorful">colorful</option>
+            </select>
+            <input className={styles.input} type="text" name="title" value={title} readOnly/>
+            <input className={styles.input} type="text" name="email" value={email} readOnly/>
+            <textarea className={styles.textarea} name="message" value={message} readOnly/>
+            <div className={styles.fileInput}>
+                <ImageFileInput />
+            </div>
+            <Button name='Delete' onClick={onSubmit} />
+        </form>
+    );
+};
+
+export default CardEditForm;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
@@ -7,6 +7,41 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+    const [cards, setCards] = useState([
+        {
+            id: '1',
+            name: 'a',
+            company: 'Samsung',
+            theme: 'light',
+            title: 'Software Engineer',
+            email: 'lsh@gmail.com',
+            message: 'go for it',
+            fileName: 'lsh',
+            fileURL: null,
+        },
+        {
+            id: '2',
+            name: 'b',
+            company: 'Samsung',
+            theme: 'dark',
+            title: 'Software Engineer',
+            email: 'lsh@gmail.com',
+            message: 'go for it',
+            fileName: 'lsh',
+            fileURL: null,
+        },
+        {
+            id: '3',
+            name: 'c',
+            company: 'Samsung',
+            theme: 'colorful',
+            title: 'Software Engineer',
+            email: 'lsh@gmail.com',
+            message: 'go for it',
+            fileName: 'lsh',
+            fileURL: null,
+        }
+    ]);
     const history = useHistory();
     const onLogout = () => {
         authService.logout();
@@ -24,8 +59,8 @@ const Maker = ({ authService }) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={cards} />
+                <Preview cards={cards} />
             </div>
             <Footer />
         </section>
